69ec29dbe5c2340dc23fb860828312f79ee0d74f
alexabravo <bravo.alexa25@gmail.com>
2021-03-04 09:46:59 -0600

    Retrofit

--- app/src/main/java/com/example/pokemonapp/PokeApi.kt
+++ app/src/main/java/com/example/pokemonapp/PokeApi.kt
@@ -5,7 +5,7 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Path
 
-public interface PokemonApi {
+public interface PokeApi {
     @GET("{id}")
     fun getPokemonById(@Path("id") id:String): Call<JsonObject>
 }
--- app/src/main/java/com/example/pokemonapp/Retrofit2.kt
+++ app/src/main/java/com/example/pokemonapp/Retrofit2.kt
@@ -1,4 +1,24 @@
 package com.example.pokemonapp
 
-class Retrofit2 {
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+class Retrofit2() {
+
+
+    private val utilsUrls: PokeApi
+
+    var Utils = Utils()
+
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl(Utils.URL_BASE)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        utilsUrls = retrofit.create(PokeApi::class.java)
+    }
+
+    fun getService(): PokeApi{
+        return utilsUrls
+    }
 }
